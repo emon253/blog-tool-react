5830ea8743bdb5bc354589aea1d48301d69f2457
emon253 <emonchowdhury253@gmail.com>
2023-11-16 21:36:01 +0600

    Change couple of forms styling

--- src/pages/blog/ViewBlog.jsx
+++ src/pages/blog/ViewBlog.jsx
@@ -34,38 +34,36 @@ export default function ViewBlog() {
   return (
     <Container>
       <div className={style.blogPage}>
+        <div className={style.categories}>
+          <h3>Categories</h3>
+          <ViewCategories />
+        </div>
         <div className={style.blogs}>
-          {/* search blog form and create blog  */}
           <CreateBlog />
-          <Form.Group className="text-center mt-1" controlId="blogSearch">
+          <Form.Group
+            className={`text-center ${style.blogSearchForm}`}
+            controlId="blogSearch"
+          >
             <div className="d-flex justify-content-center align-items-center">
               <Form.Control
                 type="text"
                 placeholder="Enter keywords to search blog"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
-                style={{ width: "50%"}}
-                className="flex-grow-1 me-2 me-md-0 "
-
+                className="flex-grow-1 me-2 me-md-0"
               />
-              <Button className="m-2" variant="primary" size="md" onClick={handleSearch}>
+              <Button className="m-1" variant="primary" onClick={handleSearch}>
                 Search
               </Button>
             </div>
           </Form.Group>
-          {/* search blog form and create blog  */}
 
-          {/* retrieving all public blogs starts */}
           <Suspense fallback={<div>Loading...</div>}>
             {posts.map((post) => (
               <PostCard key={post.id} post={post} />
             ))}
           </Suspense>
         </div>
-        <div className={style.categories}>
-          <ViewCategories />
-        </div>
-        {/* Lazy loading.... */}
       </div>
     </Container>
   );
